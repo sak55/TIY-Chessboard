cdaa8062de22fbdca2f7266b211b2791e74a4bef
Adam Karbiener <adamkarb@gmail.com>
2015-01-29 16:34:51 -0500

    edited the index to clean up the look and added the array to our main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+var board = [
+    ['R','N','B','Q','K','B','N','R'],
+    ['P','P','P','P','P','P','P','P'],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    ['p','p','p','p','p','p','p','p'],
+    ['r','n','b','q','k','b','n','r'] ];
 
+console.log(board.join('\n') + '\n\n');
+
+// Move King's Pawn forward 2
+
+board[4][4] = board[6][4];
+board[6][4] = ' ';
+console.log(board.join('\n'));
+
+
+//Move Queen's Pawn forward 1
+board[2][3] = board[1][3];
+board[1][3] = ' ';
+console.log(board.join('\n'));
+
+
+//I am lord of the first two moves in chess
+
+board[5][2] = board[7][1];
+board[7][1] = ' ';
+console.log(board.join('\n'));
+
+//I don't know why it works but it does GO ME
