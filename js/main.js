2528fc5b0ebddd20286f9d0a166902dfebac8f09
Sam Kauffman <samuelkauffman@gmail.com>
2015-01-30 15:16:37 -0500

    printing the board twice

--- js/main.js
+++ js/main.js
@@ -1,69 +1,169 @@
+function print(board) {
+    document.write('<table>');
+    
+    var head = "<th></th>",
+        cell = "<td></td>",
+        row = "<tr></tr>";
+    
+    
+    document.write("<thead>");
+    _.times(10,function(){
+        document.write(head);});
+    document.write("</thead>");
+            
+    _.times(8, function(){
+        document.write("<tr>");
+        _.times(10, function(){
+            document.write(cell);});
+        document.write("</tr>");});
+    
+    document.write("<thead>");
+    _.times(10,function(){
+        document.write(head);});
+    _.times(1, function(){
+    document.write("</thead>");
+    };
+
+print();
+
+    
+//function print(board){
+//    console.log(board.join('\n') + '\n\n')
+//    
+//    _.forEach(board, [])
+//}
+//
+//function move(fromY, fromX, toY, toX){
+//    board[toY][toX] = board[fromY][fromX];
+//    board[fromY][fromX] = ' ';
+//
+//    //    console.log(board.join('\n'));
+//        print(board);
+//    }
+//print(board);
+//console.log('<p class="rook"></p>');
+    
 //Turn your print(board) function into a toString(board) function that returns String rather than logging it. Replace the outerHTML of #chessboard with the result of toString(board) and ditch all that HTML!
 
-var board0 = [
-    [' ','A','B','C','D','E','F','G','H',' '],
-    ['8','R','N','B','Q','K','B','N','R','8'],
-    ['7','P','P','P','P','P','P','P','P','7'],
-    ['6',' ',' ',' ',' ',' ',' ',' ',' ','6'],
-    ['5',' ',' ',' ',' ',' ',' ',' ',' ','5'],
-    ['4',' ',' ',' ',' ',' ',' ',' ',' ','4'],
-    ['3',' ',' ',' ',' ',' ',' ',' ',' ','3'],
-    ['2','p','p','p','p','p','p','p','p','2'],
-    ['1','r','n','b','q','k','b','n','r','1'],
-    [' ','A','B','C','D','E','F','G','H',' ']];
+//var board0 = [
+//    [' ','A','B','C','D','E','F','G','H',' '],
+//    ['8','R','N','B','Q','K','B','N','R','8'],
+//    ['7','P','P','P','P','P','P','P','P','7'],
+//    ['6',' ',' ',' ',' ',' ',' ',' ',' ','6'],
+//    ['5',' ',' ',' ',' ',' ',' ',' ',' ','5'],
+//    ['4',' ',' ',' ',' ',' ',' ',' ',' ','4'],
+//    ['3',' ',' ',' ',' ',' ',' ',' ',' ','3'],
+//    ['2','p','p','p','p','p','p','p','p','2'],
+//    ['1','r','n','b','q','k','b','n','r','1'],
+//    [' ','A','B','C','D','E','F','G','H',' ']];
+//
+//
+//
+//function print(board0){
+//    console.log(board.join('\n') + '\n\n')
+//}
+//
+//function move(fromY, fromX, toY, toX){
+//        board0[toY][toX] = board0[fromY][fromX];
+//        board0[fromY][fromX] = ' ';
+//
+//    //    console.log(board.join('\n'));
+//        print(board0);
+//    }
+//
+//
+//print(board0);
+//
+//var board = [
+//    ['<div class="container"><table><thead><th></th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th><th></th></thead>'],
+//    [''],
+//    [''],
+//    [''],
+//    [''],
+//    [''],
+//    [''],
+//    [''],
+//    [''],
+//    ['<thead><th></th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th><th></th></thead>'],
+//    ['</table></div>']
+//]
+//     
 
 
-
-function print(board0){
-    console.log(board.join('\n') + '\n\n')
-}
-
-function move(fromY, fromX, toY, toX){
-        board0[toY][toX] = board0[fromY][fromX];
-        board0[fromY][fromX] = ' ';
-
-    //    console.log(board.join('\n'));
-        print(board0);
-    }
-
-
-print(board0);
-console.log('<p class="rook"></p>');
-
-var board = [
-    ['<div class="container"><table><thead><th></th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th><th></th></thead>'],
-    [''],
-    [''],
-    [''],
-    [''],
-    [''],
-    [''],
-    [''],
-    [''],
-    ['<thead><th></th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th><th></th></thead>'],
-    ['</table></div>']
-]
-            
-
-
-function print(board){
-    console.log(board.join('\n') + '\n\n')
-}
-
-function move(fromY, fromX, toY, toX){
-    board[toY][toX] = board[fromY][fromX];
-    board[fromY][fromX] = ' ';
-
-    //    console.log(board.join('\n'));
-        print(board);
-    }
-
-
-print(board);
-console.log('<p class="rook"></p>');
-
-
-
+//
+//<html>
+//  <body>
+//    <table id="chessboard-kinda">
+//      <thead>
+//        <tr>
+//          <td></td>
+//          <th> a </td>
+//          <th> b </td>
+//          <th> c </td>
+//          <th> d </td>
+//          <th> e </td>
+//          <th> f </td>
+//          <th> g </td>
+//          <th> h </td>
+//        </tr>
+//      </thead>
+//      <tbody>
+//        <tr>
+//          <th> 1 </th>
+//          <td class="piece"> R </td>
+//          <td class="piece"> N </td>
+//          <td class="piece"> B </td>
+//          <td class="piece"> Q </td>
+//          <td class="piece"> K </td>
+//          <td class="piece"> B </td>
+//          <td class="piece"> N </td>
+//          <td class="piece"> R </td>
+//        </tr>
+//      </tbody>
+//    </table>
+//    <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js"></script>
+//    <script>
+//
+//      var table = document.body.children[0],
+//          thead = document.body.children[0].children[0],
+//          tbody = document.body.children[0].children[1];
+//
+//      console.assert(table.tagName == 'TABLE');
+//      console.assert(thead.tagName == 'THEAD');
+//      console.assert(tbody.tagName == 'TBODY');
+//
+//      var chessboard = document.getElementById('chessboard-kinda');
+//
+//      console.assert(chessboard === table);
+//      // Shouldn't these be the same?
+//
+//      // Getting all the rows...
+//      //
+//      var rows = document.getElementsByTagName('tr');
+//      // All the `<tr>` tags...
+//
+//      console.assert(rows.length === 2);
+//      // 1 `<tr>` in `<thead>` + 1 `<tr>` in `<tbody>`
+//
+//      // Getting the row that is a child of `<tbody>`...
+//      var bodyRow = _.find(rows, { 'parentElement': tbody });
+//
+//      console.assert(bodyRow === tbody.firstChild);
+//      // Those should be the same, too, right?
+//
+//      // Getting some elements via `getElementByClassName`
+//      var cells = document.getElementsByClassName('piece');
+//
+//      console.assert(cells.length === 9);
+//      // 1 `<td>` in `<thead>` + 8 `<tr>` in `<tbody>`
+//
+//      // Getting the `<td>`'s that are children of `<tbody>`
+//      var pieces = _.filter(cells, { 'parentElement': bodyRow });
+//
+//      console.assert(pieces.length === 8);
+//    </script>
+//  </body>
+//</html>
 
 
 //
